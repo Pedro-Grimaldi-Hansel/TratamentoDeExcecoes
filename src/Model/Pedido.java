27c8f2207a6902e5ce46f6800447d8decdfc7dc1
Pedro Grimaldi <pedroghansel@gmail.com>
2023-02-06 19:40:10 -0300

    Getters e setters criados para todas as classes

--- src/Model/Pedido.java
+++ src/Model/Pedido.java
@@ -9,4 +9,52 @@ public class Pedido {
     private String data;
     private Cliente cliente;
     private ArrayList<Produto> produtos;
+
+    public int getPk_pedido() {
+        return pk_pedido;
+    }
+
+    public void setPk_pedido(int pk_pedido) {
+        this.pk_pedido = pk_pedido;
+    }
+
+    public String getCod_pedido() {
+        return cod_pedido;
+    }
+
+    public void setCod_pedido(String cod_pedido) {
+        this.cod_pedido = cod_pedido;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public ArrayList<Produto> getProdutos() {
+        return produtos;
+    }
+
+    public void setProdutos(ArrayList<Produto> produtos) {
+        this.produtos = produtos;
+    }
 }
--- src/Model/Produto.java
+++ src/Model/Produto.java
@@ -11,4 +11,60 @@ public class Produto {
     private int unidade;
     private double preco_unitario;
     private ArrayList<Pedido> pedidos;
+
+    public int getPk_produto() {
+        return pk_produto;
+    }
+
+    public void setPk_produto(int pk_produto) {
+        this.pk_produto = pk_produto;
+    }
+
+    public String getCod_produto() {
+        return cod_produto;
+    }
+
+    public void setCod_produto(String cod_produto) {
+        this.cod_produto = cod_produto;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getUnidade() {
+        return unidade;
+    }
+
+    public void setUnidade(int unidade) {
+        this.unidade = unidade;
+    }
+
+    public double getPreco_unitario() {
+        return preco_unitario;
+    }
+
+    public void setPreco_unitario(double preco_unitario) {
+        this.preco_unitario = preco_unitario;
+    }
+
+    public ArrayList<Pedido> getPedidos() {
+        return pedidos;
+    }
+
+    public void setPedidos(ArrayList<Pedido> pedidos) {
+        this.pedidos = pedidos;
+    }
 }
